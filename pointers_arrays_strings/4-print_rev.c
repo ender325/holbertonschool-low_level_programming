e36feaaf6930f62719920a6de49ce308ca53aa2b
ender325 <darion921@gmail.com>
2026-07-25 22:24:47 -0400

    Correcting Task 4.

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,14 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int index = 0; /* Initialize index to 0 */
+	int len = 0;
+	int index = 0;
 
-    while (s[index++])
-        len++;
+	while (s[index++])
+		len++;
 
-    for (index = len - 1; index >= 0; index--)
-        _putchar(s[index]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
