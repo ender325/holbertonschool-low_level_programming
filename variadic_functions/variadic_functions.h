3a325f7565c750378e348b40793338ce8f90a8a9
ender325 <darion921@gmail.com>
2026-08-16 21:03:06 -0400

    Trying this again.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,22 +1,23 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
-int _putchar(char c);
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct format_types - Struct format_types
- * @identifier: The conversion specifier
- * @f: The function pointer
- */
-typedef struct format_types
-{
-	char *identifier;
-	void (*f)(char *separator, va_list args);
-} f_dt;
 
 #endif
