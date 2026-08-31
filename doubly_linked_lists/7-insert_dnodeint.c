2174d9ffdf1f1c45aee7495a31e3a75262a35a19
ender325 <darion921@gmail.com>
2026-08-30 21:15:50 -0400

    Tryng again.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,75 +1,43 @@
 #include "lists.h"
-#include <stdlib.h>
-
 /**
- * create_dnode - creates a new DLL node with data
- * @n: data to add to node
+ * *insert_dnodeint_at_index - function inserts new node at given position
  *
- * Return: pointer to newly allocated/populated node
- */
-dlistint_t *create_dnode(const int n)
-{
-	dlistint_t *new_node = NULL;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	return (new_node);
-}
-
-/**
- * insert_dnodeint_at_index - inserts a node at position
- * in a doubly linked list
- * @h: double pointer to the head, so we can modify if needed
- * @idx: index to insert new node at
- * @n: data to add to new node
+ *@h: pointer to pointer of head of linked list
+ *@idx: unsigned int index position
+ *@n: int data
  *
- * Return: pointer to new element, NULL on failure.
+ * Return: address of new element or NULL if failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+	dlistint_t *currentnode;
+	dlistint_t *newnode;
 
-	new_node = create_dnode(n);
-	if (!new_node)
-		return (NULL);
-	if (!h || !(*h)) /* NULL DLL */
-		*h = new_node;
-	else /* DLL exists */
+	while (*h)
 	{
-		temp = *h;
-		/* advance to pos of idx in DLL */
-		while (idx != i++ && temp->next)
-			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i) /* only happens when at end of DLL */
+		newnode = malloc(sizeof(dlistint_t));
+		currentnode = *h;
+		newnode->n = n;
+
+		if (idx == 0)
 		{
-			temp->next = new_node;
-			new_node->prev = temp;
+			newnode->next = NULL;
+			return (newnode);
 		}
-		else if (idx == i - 1) /* insert at head or middle */
+		while (currentnode->next != NULL && idx - 1 > 0)
 		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else /* head of LL */
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
+			currentnode = currentnode->next;
+			idx--;
 		}
-		else /* index is out of range */
+		if (currentnode->next == NULL)
 		{
-			free(new_node);
+			free(newnode);
 			return (NULL);
 		}
+		newnode->next = currentnode->next;
+		currentnode->next = newnode;
+		return (newnode);
 	}
-	return (new_node);
+	return (NULL);
 }
