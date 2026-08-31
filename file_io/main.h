219721e10adc78714f83cd16615e0ac44fb71ae2
ender325 <darion921@gmail.com>
2026-08-30 23:41:51 -0400

    Add Task 0 of File IO

---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stddef.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+/*Structs*/
+/*Prototypes*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
