fba9eecb5ca8d2f3b4ffe5df91028f726046b572
ender325 <darion921@gmail.com>
2026-08-30 23:45:29 -0400

    Trying again.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,42 +1,32 @@
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
-  * create_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * create_file - creates a file and copies text into it
+ * @filename: the filename to create
+ * @text_content: text to copy into new file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created, or written,
+ * or write fails, etc).
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, err, len;
 
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
-
+	while (text_content && *(text_content + len))
+		len++;
+	err = write(fd, text_content, len);
 	close(fd);
+	if (err < 0)
+		return (-1);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
